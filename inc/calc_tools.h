2b88feca1cd4ef07e279e470f7cae720271833f5
Isaac Anderson <istwasanders@gmail.com>
2026-09-11 20:04:11 -0400

    Replaced old target_index function, made percolate function

--- inc/calc_tools.h
+++ inc/calc_tools.h
@@ -1,8 +1,11 @@
 #ifndef _CALC_TOOLS_H_
 #define _CALC_TOOLS_H_
 
+#include "resistor_calculator.h"
+
 double norm(int r1, int r2);
 int target_index(int target, int* list, int list_len);
 int tgt_idx(int target, int *list, int list_len);
+void resistor_percolate(struct res_calc *divider_list, int list_len);
 
 #endif
--- src/calc_tools.c
+++ src/calc_tools.c
@@ -1,36 +1,24 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <uchar.h>
 #include <math.h>
 #include "calc_tools.h"
+#include "resistor_calculator.h"
 
+// This is a "normalization" function
+// It returns the ratio of two resistances but between 1 and 10
+// Its arguments will always be between 100 and 999
 double norm(int r1, int r2)
 {
   if (r1 < r2) return 10.0 * r1 / r2;
   else return (double)r1 / r2;
 }
 
-int target_index(int target, int* list, int list_len)
-{
-  int index = 0;
-  int bit = list_len >> 1;
-  int test;
-
-  while (bit > 0) {
-    test = index | bit;
-    if (target >= list[test]) index = test;
-    bit = bit >> 1;
-  }
-
-  return index;
-}
-
 // This is basically a binary search
 // Except it returns the index of the greatest resistor
 // that is less than or equal to <target>
-int tgt_idx(int target, int *list, int list_len){
+int target_index(int target, int *list, int list_len){
     int left = 0;
     int right = list_len - 1;
     int middle;
@@ -42,4 +30,18 @@ int tgt_idx(int target, int *list, int list_len){
         else right = middle - 1;
     }
     return middle;
+}
+
+// Percolation function, puts the resistor pair under evaluation into
+// its proper position in the list
+void resistor_percolate(struct res_calc *divider_list, int list_len){
+    struct res_calc temp;
+    list_len--;
+    while((list_len > 0) && (divider_list[list_len].rnorm < divider_list[list_len-1].rnorm)){
+        // Swap divider positions
+        temp = divider_list[list_len];
+        divider_list[list_len] = divider_list[list_len - 1];
+        divider_list[list_len - 1] = temp;
+        list_len--;
+    }
 }
